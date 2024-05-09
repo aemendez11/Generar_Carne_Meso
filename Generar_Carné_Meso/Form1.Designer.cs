e5f2155a4a266221c9028d38324d67aa0ed18a7d
LENOVO <LENOVO@LAPTOP-P1U8KGS8>
2024-05-08 21:04:14 -0600

    Agregar archivos de proyecto.

---
+++ Generar_Carné_Meso/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Generar_Carné_Meso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.PictureBox pictureBox1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtContenido = new System.Windows.Forms.TextBox();
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNuevo = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = global::Generar_Carné_Meso.Properties.Resources.Logo_meso_;
+            pictureBox1.Location = new System.Drawing.Point(0, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(67, 57);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(361, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar Codigo Qr";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtContenido
+            // 
+            this.txtContenido.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContenido.Location = new System.Drawing.Point(17, 100);
+            this.txtContenido.Name = "txtContenido";
+            this.txtContenido.Size = new System.Drawing.Size(266, 37);
+            this.txtContenido.TabIndex = 1;
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.Location = new System.Drawing.Point(33, 161);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(322, 398);
+            this.pbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFoto.TabIndex = 2;
+            this.pbFoto.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese su Carné";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lime;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 579);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(718, 102);
+            this.panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(546, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Universidad Mesoamericana 2024 Sede Quetzaltenango";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(331, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Carné nuevo ";
+            // 
+            // txtNuevo
+            // 
+            this.txtNuevo.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNuevo.Location = new System.Drawing.Point(336, 100);
+            this.txtNuevo.Name = "txtNuevo";
+            this.txtNuevo.Size = new System.Drawing.Size(266, 37);
+            this.txtNuevo.TabIndex = 6;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(361, 253);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(163, 63);
+            this.btnLimpiar.TabIndex = 8;
+            this.btnLimpiar.Text = "Limpiar\r\n";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 658);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNuevo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbFoto);
+            this.Controls.Add(this.txtContenido);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Carné Meso";
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtContenido;
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNuevo;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Generar_Carné_Meso/Form1.cs
@@ -0,0 +1,75 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generar_Carné_Meso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string carneActual = txtContenido.Text;
+            string nuevoCarne = GenerarNuevoCarne(carneActual);
+            txtNuevo.Text = nuevoCarne;
+            GenerarCodigoQR(nuevoCarne);
+        }
+
+        private string GenerarNuevoCarne(string carneActual)
+        {
+            int[] vectorControl = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            if (carneActual.Length != 9)
+            {
+                MessageBox.Show("El carné debe tener exactamente 9 dígitos.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return "";
+            }
+
+            int[] digitos = new int[9];
+            for (int i = 0; i < 9; i++)
+            {
+                if (!int.TryParse(carneActual[i].ToString(), out digitos[i]))
+                {
+                    MessageBox.Show("El carné debe contener solo dígitos numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return "";
+                }
+            }
+
+            int suma = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                suma += digitos[i] * vectorControl[i];
+            }
+
+            int digitoVerificacion = (11 - (suma % 11)) % 11;
+
+            return carneActual + digitoVerificacion;
+        }
+
+        private void GenerarCodigoQR(string texto)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(texto, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            pbFoto.Image = qrCode.GetGraphic(5); // Tamaño del código QR
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtContenido.Text = "";
+            txtNuevo.Text = "";
+            pbFoto.Image = null;
+        }
+    }
+}
